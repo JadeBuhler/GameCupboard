5edcb403cf69e9d53eb5ba439b612164dcb9b8ce
Jade <buhler.jb.jb@gmail.com>
2017-11-14 03:13:38 +0000

    Minor styling on the board games index page.

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -2,4 +2,8 @@ class BoardGamesController < ApplicationController
     def index
         @games = BoardGame.all
     end
+
+    def filterByNew
+        @games = BoardGame.where(status: 'New')
+    end
 end
