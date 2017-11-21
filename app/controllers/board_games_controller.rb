b8ddba5185e5587f184119023483750238839840
Jade <buhler.jb.jb@gmail.com>
2017-11-21 18:54:20 +0000

    Got category filtering working. Users can now select a category from a drop down list and click 'Filter' to be taken to a page containing products of that category type. If 'default' is chosen as a category the user is redirected back to the original products page that displays all products.

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -12,11 +12,16 @@ class BoardGamesController < ApplicationController
 
     def search_results
         @wildcard_keywords = '%' + params[:search_keywords] + '%'
-        @games = BoardGame.where("name LIKE ?", @wildcard_keywords)
+        @games = BoardGames.where("name LIKE ?", @wildcard_keywords)
     end
 
     def filter_results
-        @filtered_category = params[:filter_by] + '"'
-        @games = BoardGame.where("category =", @filtered_category)
+        @filtered_category = params[:filter_by]
+
+        if @filtered_category == ""
+            redirect_to products_path
+        else
+            @games = BoardGame.where("category_id = ?", @filtered_category)
+        end
     end
 end
