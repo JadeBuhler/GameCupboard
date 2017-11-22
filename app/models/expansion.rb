0f14e51ebb58b91b813bb01a35d5b9631f969f54
Jade <buhler.jb.jb@gmail.com>
2017-11-22 05:15:13 +0000

    Began styling the application with my own styles (Screw bootstrap). Removed bulky css files, all css is now handled in the application.scss file.

--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -4,6 +4,6 @@ class Expansion < ApplicationRecord
     validates :price, numericality: true
     has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
 validates_attachment :image, :presence => true,
-:content_type => { :content_type => "image/jpg", :content_type => "image/jpeg" },
+:content_type => { :content_type => "image/jpeg" },
 :size => { :in => 0..1000.kilobytes }
 end
